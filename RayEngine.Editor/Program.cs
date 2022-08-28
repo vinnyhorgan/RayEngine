dbfe7d0dd4948e3c1499b0935406e8649c74767f
Vinny Horgan <vinnyhorgan@gmail.com>
2022-08-28 15:03:27 +0200

    Added basic editor gui structure

--- RayEngine.Editor/Program.cs
+++ RayEngine.Editor/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Raylib_cs;
+using ImGuiNET;
+using System.Numerics;
 
 using static Raylib_cs.Raylib;
 using static Raylib_cs.Color;
@@ -9,17 +11,23 @@ namespace RayEngine.Editor
 	{
 		public static void Main()
 		{
-			ScreenManager manager = new ScreenManager();
-
-			manager.change(new EditorScreen());
-
 			const int width = 1000;
 			const int height = 600;
 
 			SetTraceLogLevel(TraceLogLevel.LOG_NONE);
-			SetConfigFlags(ConfigFlags.FLAG_MSAA_4X_HINT);
+			SetConfigFlags(ConfigFlags.FLAG_MSAA_4X_HINT | ConfigFlags.FLAG_WINDOW_RESIZABLE);
 			InitWindow(width, height, "Ray Engine Editor");
 			SetTargetFPS(60);
+
+			ScreenManager manager = new ScreenManager();
+			manager.change(new EditorScreen());
+
+			ImguiController controller = new ImguiController();
+
+			ImGuiIOPtr io = ImGui.GetIO();
+			io.ConfigFlags = ImGuiConfigFlags.DockingEnable;
+
+			controller.Load(width, height);
 
 			while (!WindowShouldClose())
 			{
@@ -27,16 +35,24 @@ namespace RayEngine.Editor
 
 				manager.current.Update(dt);
 
+				controller.Update(dt);
+
 				BeginDrawing();
 
-				ClearBackground(BLACK);
+					ClearBackground(BLACK);
 
-				manager.current.Draw();
+					ImGui.DockSpaceOverViewport();
+
+					manager.current.Draw();
+
+					controller.Draw();
 
 				EndDrawing();
 			}
 
 			manager.current.Destroy();
+
+			controller.Dispose();
 
 			CloseWindow();
 		}
--- RayEngine.Editor/Screens/EditorScreen.cs
+++ RayEngine.Editor/Screens/EditorScreen.cs
@@ -1,30 +1,129 @@
 using Raylib_cs;
+using ImGuiNET;
+using System;
+using System.Numerics;
 
 using static Raylib_cs.Raylib;
 using static Raylib_cs.Color;
+using static Raylib_cs.MaterialMapIndex;
 
 namespace RayEngine.Editor
 {
 	class EditorScreen : Screen
 	{
-		public override void Start()
-		{
+		RenderTexture2D viewport;
+		Camera3D camera;
 
+		Model testModel;
+
+		private void ViewportDraw()
+		{
+			BeginMode3D(camera);
+
+			ClearBackground(DARKBLUE);
+
+			DrawGrid(10, 1.0f);
+
+			DrawModel(testModel, Vector3.Zero, 0.5f, WHITE);
+
+			EndMode3D();
+		}
+
+		public unsafe override void Start()
+		{
+			viewport = LoadRenderTexture(640, 480);
+
+			camera.position = new Vector3(10.0f, 20.0f, 10.0f);
+			camera.target = new Vector3(0.0f, 5.0f, 0.0f);
+			camera.up = new Vector3(0.0f, -1.0f, 0.0f);
+			camera.fovy = 45.0f;
+			camera.projection = CameraProjection.CAMERA_PERSPECTIVE;
+
+			SetCameraMode(camera, CameraMode.CAMERA_ORBITAL);
+
+			testModel = LoadModel("Assets/models/church.obj");
+			Texture2D texture = LoadTexture("Assets/models/church_diffuse.png");
+			testModel.materials[0].maps[(int)MATERIAL_MAP_DIFFUSE].texture = texture;
 		}
 
 		public override void Update(float dt)
 		{
-
+			UpdateCamera(ref camera);
 		}
 
 		public override void Draw()
 		{
-			DrawText("Hello, World!", 10, 10, 50, WHITE);
-		}
+			BeginTextureMode(viewport);
 
-		public override void Draw3d()
-		{
+			ViewportDraw();
 
+			EndTextureMode();
+
+			if (ImGui.BeginMainMenuBar())
+			{
+				if (ImGui.BeginMenu("System"))
+				{
+
+
+					ImGui.EndMenu();
+				}
+
+				if (ImGui.BeginMenu("Scene"))
+				{
+
+
+					ImGui.EndMenu();
+				}
+
+				if (ImGui.BeginMenu("Entity"))
+				{
+
+
+					ImGui.EndMenu();
+				}
+
+				if (ImGui.BeginMenu("Help"))
+				{
+
+
+					ImGui.EndMenu();
+				}
+
+				ImGui.EndMainMenuBar();
+			}
+
+			if (ImGui.Begin("Viewport"))
+			{
+				Vector2 size = ImGui.GetWindowSize();
+				size.Y -= 40;
+
+				ImGui.Image(new IntPtr(viewport.texture.id), size);
+			}
+			ImGui.End();
+
+			if (ImGui.Begin("Scene"))
+			{
+
+			}
+			ImGui.End();
+
+			if (ImGui.Begin("Inspector"))
+			{
+
+			}
+			ImGui.End();
+
+			if (ImGui.Begin("Filesystem"))
+			{
+
+			}
+			ImGui.End();
+
+			if (ImGui.Begin("Console"))
+			{
+
+			}
+			ImGui.End();
 		}
 
 		public override void Destroy()
